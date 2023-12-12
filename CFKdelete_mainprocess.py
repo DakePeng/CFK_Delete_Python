aa8fe125e7ed999f61338ecacf93933e4a7ad2d2
Dake Peng <pengd@carleton.edu>
2023-12-11 21:36:42 -0600

    added comment

--- CFKdelete_mainprocess.py
+++ CFKdelete_mainprocess.py
@@ -3,6 +3,8 @@ from datetime import date
 
 date = date.today()
 num_times_to_run = 10
+#it takes roughly 5 minutes to complete each run that deletes 500 files. 
+#this means if we leave the program running 14 consecutive days, it will be able to delete all 2 million files
 
 for i in range(num_times_to_run):
     # Run subprocess and redirect output to a text file
