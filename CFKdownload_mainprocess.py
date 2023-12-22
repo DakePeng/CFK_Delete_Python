76425f8fedf68d737c092119e7f518dece89ed45
Dake Peng <pengd@carleton.edu>
2023-12-22 14:36:25 -0600

    added download function

---
+++ CFKdownload_mainprocess.py
@@ -0,0 +1,45 @@
+''' 
+    By Dake Peng, Carleton 25'.
+    Main deletion program for the CFK project
+    Runs "CFKdelete_subprocess.py", and creates a log file to record successful deletions of each file.
+    creates a log to record all unsuccessful deletions each day.
+
+    !!! Check all comments surrounded by exclamation marks "!!!" before production. !!!
+'''
+import subprocess
+from datetime import date
+import os
+'''
+    !!! verify this number (num_times_to_run) before production !!!
+    it takes roughly 45 seconds to download 1 GB worth of files
+    this means if we leave the program running 14 consecutive days, it will be able to delete all 2 million files
+    
+    To be tested:
+    Google has a quota of 12,000 requests per minute. That is the max # of deletions we can do with this code.
+    (this may not effect our code at all)
+    https://developers.google.com/drive/api/guides/limits
+'''
+#number of times to run the subprocess
+num_times_to_run = 1
+
+
+'''
+    Run subprocess and redirect output to a text file
+'''
+if __name__ == '__main__':
+    date = date.today()
+    error_file = str(date) + '_error_log.txt'
+    with open(error_file, "w") as error_log:
+        i = 0
+        for j in range(num_times_to_run):
+            #get today's date
+            date = date.today()
+            folder = str(date) + "_download_log"
+            if not os.path.exists(folder):
+                i = 0
+                os.makedirs(folder)
+            output_file = "./" + folder + '/' + str(date) + '_attempt_' + str(i) + '_log.txt'
+            with open(output_file, "w") as std_log:
+                #!!! fix "testsubprocess.py" before production !!!
+                subprocess.run(["python", "CFKdownload_subprocess.py"], stdout= std_log, stderr= error_log)
+            i += 1
